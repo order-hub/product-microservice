046c5dba7f7394132b0b6dd58df242718804c10e
yyubin <hazing120@gmail.com>
2025-02-26 04:31:46 +0900

    test: ProductDiscountTest 추가된 로직 테스트 추가

--- src/test/java/org/orderhub/pr/discount/domain/ProductDiscountTest.java
+++ src/test/java/org/orderhub/pr/discount/domain/ProductDiscountTest.java
@@ -3,7 +3,10 @@ package org.orderhub.pr.discount.domain;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
+import org.orderhub.pr.discount.dto.request.ProductDiscountUpdateRequest;
 import org.orderhub.pr.product.domain.Product;
+
+import java.time.Instant;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.*;
@@ -38,5 +41,56 @@ class ProductDiscountTest {
     @Test
     void testIsThresholdPriceDiscount_ReturnsFalse() {
         assertThat(productDiscount.isThresholdPriceDiscount()).isFalse();
+    }
+
+    @Test
+    void testUpdateProductDiscount() {
+        // given
+        Product newProduct = mock(Product.class);
+        when(newProduct.getId()).thenReturn(2L);
+
+        ProductDiscountUpdateRequest updateRequest = ProductDiscountUpdateRequest.builder()
+                .discountValue(3000)
+                .thresholdQuantity(5)
+                .discountUnitPrice(3500)
+                .startDate(Instant.now().plusSeconds(3600)) // 1시간 후
+                .endDate(Instant.now().plusSeconds(7200)) // 2시간 후
+                .build();
+
+        // when
+        productDiscount.update(newProduct, DiscountType.PERCENTAGE, updateRequest);
+
+        // then
+        assertThat(productDiscount.getProduct()).isEqualTo(newProduct);
+        assertThat(productDiscount.getDiscountType()).isEqualTo(DiscountType.PERCENTAGE);
+        assertThat(productDiscount.getDiscountValue()).isEqualTo(3000);
+        assertThat(productDiscount.getThresholdQuantity()).isEqualTo(5);
+        assertThat(productDiscount.getDiscountUnitPrice()).isEqualTo(3500);
+        assertThat(productDiscount.getStartDate()).isEqualTo(updateRequest.getStartDate());
+        assertThat(productDiscount.getEndDate()).isEqualTo(updateRequest.getEndDate());
+    }
+
+    @Test
+    void testDeleteProductDiscount() {
+        // when
+        productDiscount.delete();
+
+        // then
+        assertThat(productDiscount.getStatus()).isEqualTo(DiscountStatus.DELETED);
+        assertThat(productDiscount.isDeleted()).isTrue();
+    }
+
+    @Test
+    void testRestoreProductDiscount() {
+        // given
+        productDiscount.delete();
+        assertThat(productDiscount.isDeleted()).isTrue();
+
+        // when
+        productDiscount.restore();
+
+        // then
+        assertThat(productDiscount.getStatus()).isEqualTo(DiscountStatus.ACTIVE);
+        assertThat(productDiscount.isDeleted()).isFalse();
     }
 }
